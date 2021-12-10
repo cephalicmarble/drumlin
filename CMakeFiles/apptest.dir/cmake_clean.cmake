09353dda1336ba136cf3031971be62d773b727e7
Archie Campbell <cephalicmarble@hotmail.com>
2021-12-10 14:45:54 +0000

    CMakeLists.txt for tests. tests/... currently contains two copies of gtest boilerplate.

---
+++ CMakeFiles/apptest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apptest.dir/tests/apptest/apptest.cpp.o"
+  "CMakeFiles/apptest.dir/tests/apptest/apptest.cpp.o.d"
+  "apptest"
+  "apptest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apptest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
